49913c052428c0cab8cd9af59d6a3a1ef50bf6fa
HelgaLantaarn <Myshonysh777@gmail.com>
2025-02-04 18:41:25 +0100

    + task 1.4

---
+++ HomeworkJs1/main1_4.js
@@ -0,0 +1,5 @@
+const firstName = prompt("Введіть ім'я:");
+const lastName = prompt("Введіть прізвище:");
+const age = prompt("Вік:");
+
+console.log(`Ім'я: ${firstName} Прізвище: ${lastName} Вік: ${age}`);
