075addebc620384853a1ba010d6c343440b8988f
HelgaLantaarn <Myshonysh777@gmail.com>
2025-02-04 16:00:36 +0100

    task 1.1

---
+++ HomeworkJs1/main.js
@@ -0,0 +1,24 @@
+let a = 'hello'
+console.log(a)
+let b = 'owu'
+console.log(b)
+let c = 'com'
+console.log(c)
+let d = 'ua'
+console.log(d)
+let e = 1
+console.log(e)
+let f = 10
+console.log(f)
+let g = -999
+console.log(g)
+let h = 123
+console.log(h)
+const i = 3.14
+console.log(i)
+const j = 2.7
+console.log(j)
+let k = 16
+console.log(k)
+console.log(10>5)
+console.log(10<5)
