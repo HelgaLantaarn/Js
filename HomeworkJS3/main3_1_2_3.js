db73a02b437362f0fdb00624e85d4a38a832918b
HelgaLantaarn <Myshonysh777@gmail.com>
2025-02-11 20:06:47 +0100

    task 3 all + add 3_1_2_3

---
+++ HomeworkJS3/main3_1_2_3.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+    document.write('<div>hard in study but will be simple in work</div>');
+    
+}
+for (let i = 0; i < 10; i++) {
+    document.write(`<div>hard in study but will be simple in work ${i}</div>`)
+
+}
+
+let i = 0;
+while (i < 20) {
+    document.write('<h1>' + 'hard in study but will be simple in work' + '</h1>');
+    i++;
+}
+
---
+++ HomeworkJS3/main3_4.js
@@ -0,0 +1,6 @@
+let i = 0;
+while (i < 20) {
+    document.write(`<div>hard in study but will be simple in work ${i} </div>`);
+    i++;
+}
+
